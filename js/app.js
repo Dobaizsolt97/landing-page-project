b2dd0eb46675e1b92aa4fa5f163aa784ed7c211a
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-03-02 19:57:39 +0200

    implemented smooth scrolling and sticky navbar, also rethinked a tag generation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const navbar = document.getElementById("navbar");
 const header = document.getElementById("header");
+const links = document.querySelector(".link-list");
 
 const sections = document.getElementsByClassName("landing-section");
 
@@ -19,7 +20,44 @@ gettingLinks();
 /* populate navbar with a tags to target the page sections */
 function updateNavbar(array) {
   for (element of array) {
-    navbar.innerHTML =
-      navbar.innerHTML + `<div><a href='#${element}'>${element}</a></div>`;
+    links.innerHTML =
+      links.innerHTML +
+      // asigning the scroll function to the generated a tags
+      `<li><a onclick='smoothScroll(${element})' >${element}</a><li>`;
+  }
+}
+
+/* function that enables smooth scrolling, activated trough click */
+function smoothScroll(target) {
+  //getting the coordinate of the section where we want to scroll
+  let targetPozition = target.getBoundingClientRect().top;
+  // the current pozition
+  let startPosition = window.pageYOffset;
+  let distance = 0;
+  // determining the Y coordinate where we want to scroll based on
+  // the relationsheep between the current location and the target
+  if (targetPozition > startPosition) {
+    distance = targetPozition - startPosition;
+  } else {
+    distance = startPosition + targetPozition;
+  }
+  window.scrollTo({
+    top: distance,
+    behavior: "smooth"
+  });
+}
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+// w3school way of implementing sticky navbars
+let sticky = navbar.offsetTop;
+
+function scrollFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
   }
 }
